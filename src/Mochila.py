e5cb1b070c85af40aaa8c9b9e15f2c7ed6caa149
carlochess <carlochess@gmail.com>
2015-04-17 02:55:33 -0500

    Hecho

--- src/Mochila.py
+++ src/Mochila.py
@@ -2,5 +2,5 @@ from Objeto import Objeto
 class Mochila(Objeto):
     def __init__(self, peso, volumen):
         Objeto.__init__(self, peso, volumen)
-	def __str__(self):
-		return str(self.peso)+" "+str(self.volumen)
+    def __str__(self):
+        return str(self.peso)+" "+str(self.volumen)
