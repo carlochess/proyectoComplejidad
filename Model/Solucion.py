5cb492ab5b5c9d6fccd0919dd529172fc1b9694f
carlochess <carlochess@gmail.com>
2015-04-25 01:53:49 -0500

    Agregada clase para almacenar la solucion

---
+++ Model/Solucion.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+class Solucion:
+    def __init__(self):
+        self.numMochilas = 0
+        self.mochilas = None
+        self.items = []
+        self.nombreArchivoSol = ""
+        self.vectorSolucion = []
+        self.MatrizSolucion = [[]]
